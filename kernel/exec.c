b0270ba0776763f5936db1b6d6793c46e874b3b0
ZhuZiyi <zyzhu2001@gmail.com>
2023-03-05 17:46:47 +0800

    Pass the task of Print a page table

--- kernel/exec.c
+++ kernel/exec.c
@@ -115,6 +115,11 @@ exec(char *path, char **argv)
   p->trapframe->epc = elf.entry;  // initial program counter = main
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
+  /** 实验要求：打印第一个进程刚刚完成exec时的页表 */
+  if(p->pid == 1) {
+  	vmprint(p->pagetable);
+  }
+  /** =====================================*/
 
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
